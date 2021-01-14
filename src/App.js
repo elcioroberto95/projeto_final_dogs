f6af7503cf230987fc4d27e5220adf1e625d3ea5
elcioroberto95 <elcioroberto1995@gmail.com>
2021-01-14 17:38:55 -0300

    feat: initial structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import react from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './components/Login/Login';
 
 const App = () => {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 export default App;
